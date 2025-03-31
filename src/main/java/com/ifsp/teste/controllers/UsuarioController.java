6993aab91cb8d1ed90340bfbade9a9da67d27b02
vilasbtw <kaiquevilasbs@gmail.com>
2025-03-31 10:18:16 -0300

    Adicionando página Documentação

--- src/main/java/com/ifsp/teste/controllers/UsuarioController.java
+++ src/main/java/com/ifsp/teste/controllers/UsuarioController.java
@@ -1,22 +1,16 @@
 package com.ifsp.teste.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.ifsp.teste.TesteApplication;
 import com.ifsp.teste.models.Usuario;
 import com.ifsp.teste.services.UsuarioService;
-import com.ifsp.teste.utils.BodyParser;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UsuarioController {   
@@ -45,17 +39,29 @@ public class UsuarioController {
     public String login(
         Model model,
         @RequestParam String email,
-        @RequestParam String senha
+        @RequestParam String senha,
+        HttpSession session
     ) {
         Usuario u = usuarioService.autenticar(email, senha);
         
         if (u == null) {
-            model.addAttribute("mensagem", "email ou senha incorretos");
+            model.addAttribute("mensagem", "Email ou senha incorretos");
             return "/login";
-        } 
-
-        //TODO: criar a home!
+        }
+        // sessão p/ usuario acessar outras paginas
+        session.setAttribute("usuarioAutenticado", u); 
         return "redirect:/home";
     }
-    
+
+    @GetMapping("/documentacao")
+    public String documentacao(HttpSession session, Model model) {
+        Usuario u = (Usuario) session.getAttribute("usuarioAutenticado");
+
+        if (u == null) {
+            model.addAttribute("mensagem", "é necessário estar cadastrado!");
+            return "redirect:/login";
+        }
+
+        return "documentacao";
+    }
 }
